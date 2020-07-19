15a5b2d624a6d23d097c9c8c565252872831dc67
hansgarcia96 <hansgarcia96@gmail.com>
2020-07-19 00:25:03 -0400

    usa-map

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import USAMap from "react-usa-map";
 
 // import PostSorting from './posts/PostSorting'
 // import ImgDropAndCrop from './learn/ImgDropAndCrop'
@@ -10,12 +11,38 @@ class App extends React.Component {
     this.state = { data: null };
   }
 
+  // MAP STUFF
+  mapHandler = (event) => {
+    alert(event.target.dataset.name);
+    console.log(event);
+  };
+
+  // with the clickHandler, you can change the text that pops up in the alert
+  statesFilling = () => {
+    return {
+      NJ: {
+        fill: "navy",
+        clickHandler: () => alert("New Jersey"),
+      },
+      FL: {
+        fill: "#CC0000",
+      },
+    };
+  };
+
+// API CALL FOR FLORIDA TESTING CENTERS
   componentDidMount() {
     const apiUrl =
       "https://covid-19-testing.github.io/locations/florida/complete.json";
     fetch(apiUrl)
       .then((response) => response.json())
-      .then((data) => console.log("DATA", data));
+      .then((data) => console.log("florida testing centers", data));
+
+// USER LOCATION IN CONSOLE
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+    });
   }
 
   render() {
@@ -27,6 +54,7 @@ class App extends React.Component {
         <h1>component mounted</h1>
         <h3>states in the console:</h3>
         <h5>Florida</h5>
+        <USAMap customize={this.statesFilling()} onClick={this.mapHandler} />
       </div>
     );
   }
